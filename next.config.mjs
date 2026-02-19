a4592a3987bd6e95eb48d24f3cc2d4f4b6edff9b
maryam <thrimarym@gmail.com>
2026-02-19 15:25:16 +0330

    fix tailwind css config - remove lightningcss

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  // غیرفعال کردن بهینه‌سازی CSS که lightningcss رو می‌خواهد
+  experimental: {
+    optimizeCss: false,
+  }
 };
 
 export default nextConfig;
